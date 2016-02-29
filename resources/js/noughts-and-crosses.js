b8566c372fe9425099b4de7279d25f4987f39dee
James <jammatpic@gmail.com>
2016-02-29 23:31:15 +1100

    Added basic layout, buttons

--- resources/js/noughts-and-crosses.js
+++ resources/js/noughts-and-crosses.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
+var positions = ["top-left", "top-mid", "top-right", "mid-left", "mid-mid", "mid-right", "btm-left", "btm-mid", "btm-right"];
+var moves = []; // moves given as indexes of positions array, e.g. 1 = top-mid
 
+$(document).ready(function() {
+    $(".btn").on("click", function() {
+        var position = $(this).attr("id");
+        var validMove = true;
+        
+        if ($(this).text() != "X" && $(this).text() != "O") {
+            $("#" + position).text("X");
+        }
+    });
 });
