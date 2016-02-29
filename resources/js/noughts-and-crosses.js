d5d54df15486612559f4fb2c033a4432b1aa8fd6
James <jammatpic@gmail.com>
2016-03-01 00:21:01 +1100

    Added basic gameplay

--- resources/js/noughts-and-crosses.js
+++ resources/js/noughts-and-crosses.js
@@ -1,13 +1,19 @@
 var positions = ["top-left", "top-mid", "top-right", "mid-left", "mid-mid", "mid-right", "btm-left", "btm-mid", "btm-right"];
-var moves = []; // moves given as indexes of positions array, e.g. 1 = top-mid
+var moves = [];
 
 $(document).ready(function() {
-    $(".btn").on("click", function() {
+    $(".btn-default").on("click", function() {
         var position = $(this).attr("id");
-        var validMove = true;
+        var moveIndex = positions.indexOf(position);
         
         if ($(this).text() != "X" && $(this).text() != "O") {
             $("#" + position).text("X");
+            moves.push(moveIndex);
+            positions.splice(moveIndex, 1);
+            var compMoveIndex = Math.floor(Math.random()*positions.length);
+            $("#" + positions[compMoveIndex]).text("O");
+            moves.push(compMoveIndex)
+            positions.splice(compMoveIndex, 1);
         }
     });
 });
